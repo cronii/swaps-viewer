e5b173f67352171cd1913a0dc99ed33aa4052a9e
cronii <cronii@protonmail.com>
2023-07-24 23:35:21 -0400

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { displayToken, shortenAddress, toAddressLink, toTxLink } from '../utils';
 
 type Swap = {
@@ -15,8 +15,14 @@ type Swap = {
   token1_out: string
 };
 
+type Pair = {
+  token0_decimals: number,
+  token1_decimals: number
+};
+
 const Home = () => {
   // const [nametags, setNametags] = useState({});
+  const [pairsData, setPairsData] = useState<Pair>({token0_decimals: 18, token1_decimals: 18});
   const [swapsData, setSwapsData] = useState([]);
 
   const queryParams = new URLSearchParams(window.location.search);
@@ -25,10 +31,11 @@ const Home = () => {
   const pool = queryParams.get('pool')?.toUpperCase() || 'v2';
 
   useEffect(() => {
+    fetch(`http://localhost:5001/api/pairs?quote=${quote}&base=${base}&pool=${pool}`)
+      .then((response) => response.json())
+      .then((data) => setPairsData(data[0]))
+      .catch((error) => console.error('Error fetching data:', error));
 
-  }, []);
-
-  useEffect(() => {
     fetch(`http://localhost:5001/api/swaps?quote=${quote}&base=${base}&pool=${pool}`)
       .then((response) => response.json())
       .then((data) => setSwapsData(data))
@@ -51,6 +58,8 @@ const Home = () => {
     return nametags[address] || shortenAddress(address);
   }
 
+  let prevTxHash: string = '';
+
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
       <table>
@@ -67,17 +76,20 @@ const Home = () => {
         <tbody>
           {swapsData.map((swap: Swap) => {
             const { tx_id, block, sender_address, to_address, tx_hash, token0_in, token0_out, token1_in, token1_out } = swap;
+            const { token0_decimals, token1_decimals } = pairsData;
             const isBuy = (BigInt(swap.token0_out) > 0 && BigInt(swap.token1_in) > 0);
+            const isSameTx = prevTxHash === tx_hash;
+            prevTxHash = tx_hash;
             return (
-              <tr key={tx_id} className='text-right'>
+              <tr key={tx_id} className={`text-right ${isSameTx && 'text-red-200'}`}>
                 <td>{block}</td>
                 <td><a href={toTxLink(tx_hash)} target='_blank'>{shortenAddress(tx_hash)}</a></td>
                 <td><a href={toAddressLink(sender_address)} target='_blank'>{parseAddress(sender_address)}</a></td>
                 <td><a href={toAddressLink(to_address)} target='_blank'>{parseAddress(to_address)}</a></td>
-                {isBuy && (<td className='text-green-500'>{displayToken(token0_out)}</td>)}
-                {isBuy && (<td className='text-green-500'>{displayToken(token1_in)}</td>)}
-                {!isBuy && (<td className='text-red-500'>{displayToken(token0_in)}</td>)}
-                {!isBuy && (<td className='text-red-500'>{displayToken(token1_out)}</td>)}
+                {isBuy && (<td className='text-green-500'>{displayToken(token0_out, token0_decimals)}</td>)}
+                {isBuy && (<td className='text-green-500'>{displayToken(token1_in, token1_decimals)}</td>)}
+                {!isBuy && (<td className='text-red-500'>{displayToken(token0_in, token0_decimals)}</td>)}
+                {!isBuy && (<td className='text-red-500'>{displayToken(token1_out, token1_decimals)}</td>)}
               </tr>
             )
           })}
